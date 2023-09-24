2aa2810e8ad1bd71e0d3d04e188ae48930ff7a43
ahlamawad <aezd2010@hotmail.com>
2023-09-24 14:48:51 +0400

    first commit

---
+++ hw1.js
@@ -0,0 +1,11 @@
+
+
+
+myStudents=["ahmed","omar","enas","ghadeer","farah","eman"]
+
+
+for (let i = 0; i < myStudents.length;i++) {
+
+    myStudents[i][0]=="e"?console.log(myStudents[i]):console.log("sorry this name is not starting with your letter");
+
+}
